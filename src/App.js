08b41e1fd90776de666e440f0eee4c949a2bc220
anerie <aneriep@gmail.com>
2025-01-19 09:37:32 -0500

    added the pics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useRef } from "react";
 import "./index.css";
 import logo from './logo.png'; 
+import grace from './img/grace.jpg'
+import aroob from './img/aroob.png'
+import sophie from './img/sophie.jpeg'
+import tishya from './img/tishya.jpg'
+import vivan from './img/vivan.jpeg'
+import silly from './img/silly.jpg'
 
 const App = () => {
   // Create refs for each section
@@ -18,6 +24,28 @@ const App = () => {
     }
   };
 
+  const handleMouseEnter = (e) => {
+    const sillyImage = e.target.getAttribute("data-silly"); // Get the "silly" image path
+    if (sillyImage) {
+      e.target.src = sillyImage; // Change the src to the silly image
+    }
+  };
+  
+  const handleMouseLeave = (e) => {
+    const originalImage = e.target.getAttribute("alt"); // Use the alt text to determine the original image
+    if (originalImage === "grace") {
+      e.target.src = grace; // Reset to the original image
+    } else if (originalImage === "tishya") {
+      e.target.src = tishya;
+    } else if (originalImage === "aroob") {
+      e.target.src = aroob;
+    } else if (originalImage === "sophie") {
+      e.target.src = sophie;
+    } else if (originalImage === "vivan") {
+      e.target.src = vivan;
+    }
+  };
+  
   return (
     <div>
       {/* Navigation Buttons */}
@@ -60,7 +88,7 @@ const App = () => {
 
       <section ref={section2Ref} className="section">
         <h1>About Us</h1>
-       
+
       </section>
 
       <section ref={section3Ref} className="section">
@@ -69,6 +97,50 @@ const App = () => {
 
       <section ref={section4Ref} className="section">
         <h1>Our Team</h1>
+
+        <div className="image-container">
+          <img 
+            src={grace} 
+            alt="grace" 
+            className="img" 
+            data-silly={silly}
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+          <img 
+            src={tishya} 
+            alt="tishya" 
+            className="img" 
+            data-silly={silly} 
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+          <img 
+            src={aroob} 
+            alt="aroob" 
+            className="img" 
+            data-silly={silly}
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+          <img 
+            src={sophie} 
+            alt="sophie" 
+            className="img" 
+            data-silly={silly}
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+          <img 
+            src={vivan} 
+            alt="vivan" 
+            className="img" 
+            data-silly={silly}
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave} 
+          />
+          </div>
+
       </section>
 
       <section ref={section5Ref} className="section">
