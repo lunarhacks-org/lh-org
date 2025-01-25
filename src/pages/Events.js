9f2f96460bfa2b6f57308f29506ffd2769348979
anerie <aneriep@gmail.com>
2025-01-24 21:01:08 -0500

    playing a little more with events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Events.css";
+import lumText from "../img/lumText.png";
+import lumLogo from "../img/lumLogo.png";
+import Navbar from "../components/Navbar";
 
 const Events = () => {
   return (
     <div className="events">
-      <a href="/" className="event-button">
-        LunarHacks
-      </a>
-      <h1>Spark x Lunar Hacks</h1>
-      <p>Best workshop everrrr!!!! Please come</p>
+      <Navbar />
+      {/* FIX NAV BAR STUFF! */}
+      <div className="header-container">
+        <img src={lumLogo} alt="Lum Logo" className="lum-logo" />
+        <img src={lumText} alt="Lum Text" className="lum-text" />
+      </div>
     </div>
   );
 };
